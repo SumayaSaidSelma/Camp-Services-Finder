ea8cb9d043cdbf5f4416b63fb73b12a3b6df611a
SumayaSaidSelma <sumayasaidselma@gmail.com>
2024-10-02 11:01:20 +0200

    updated user and admin side

--- services/forms.py
+++ services/forms.py
@@ -1,12 +1,12 @@
 from django import forms
-from .models import Service 
+from .models import Service, CustomUser  
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
-        model = User
+        model = CustomUser
         fields = ['username', 'email', 'password1', 'password2']
 
 
